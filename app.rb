a95691577f6ea1e3a72445b532e5cf08d09d3269
Bhupesh <bhupesh100@gmail.com>
2020-10-28 18:21:44 +0000

    Extracting Logic to the Model 2

--- app.rb
+++ app.rb
@@ -8,21 +8,21 @@ class Battle < Sinatra::Base
     erb(:index)
   end
 
-  post "/names" do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
-    redirect "/play"
+  post '/names' do
+    $player1 = Player.new(params[:player1])
+    $player2 = Player.new(params[:player2])
+    redirect '/play'
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb :play
   end
 
   get '/attack' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb :attack
   end
 
